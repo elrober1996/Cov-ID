8ffef5ff400be47c82dfed9f68a6a53b4aae7461
elrober1996 <elrober1996@gmail.com>
2021-09-26 13:50:29 -0400

    Basic layout of the log in and the sign-up.

---
+++ COMP4400-Cov-ID/signup.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'header.php';
+?>
+
+<section class="si-form">
+    <h2>Log In</h2>
+    <div class="signup-form-form">
+        <form action="login.inc.php" method="post">
+            <input type="text" name="name" placeholder="Full Name">
+            <input type="text" name="email" placeholder="Email" >
+            <input type="text" name="uid" placeholder="Username" >
+            <input type="password" name="pwd" placeholder="Password" >
+            <input type="password" name="pwdrepeat" placeholder="Repeat Password">
+            <button type="submit" name="submit">Sign Up</button
+        </form>
+    </div>
+</section>
+
+<?php
+include_once 'footer.php';
+?>
